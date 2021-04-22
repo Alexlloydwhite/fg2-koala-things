4b4abd321f092cce6554c3039ec7ee50b7bfbe1e
Christian Back <cdirt27@gmail.com>
2021-04-22 15:16:58 -0500

    Moving stuff

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,7 +2,24 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'koala-DB',
+    host: 'localhost',
+    port: '5432',
+    max: '10',
+    idleTimeoutMillis: 30000
+});
 
+pool.on('connect', () => {
+    console.log('Postgresql, Eugalize Legalyptus!');
+});
+
+// handle errors from the DB
+pool.on('error', error => {
+    console.log('Error with postgres pool!', error);
+});
 
 // GET
 
