bb2af1c1606a721b1f7bd2cbd89a9440d4f4aec7
Alex White <alexlloydwhite@gmail.com>
2021-04-22 14:03:09 -0500

    client side get routes established, render function complete - not tested

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,8 +30,45 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  })
+  .then( function(response) {
+    console.log(response);
+    renderKoalas(response);
+  })
+  .catch( function(error) {
+    console.log(`Error in GET, ${error}`);
+  })
 } // end getKoalas
+
+function renderKoalas(koalas){
+  // empty element on the DOM
+  $('#viewKoalas').empty();
+
+  for(let i = 0; i < koalas.length; i++) {
+    let newRow = $(`
+    <tr>
+      <td>${koalas[i].name}</td>
+      <td>${koalas[i].age}</td>
+      <td>${koalas[i].gender}</td>
+      <td>${koalas[i].ready_to_transfer}</td>
+      <td>${koalas[i].notes}</td>
+      <td>
+        <button type="button" class="markReady" data-id="${koalas[i].id}">
+          Ready For Transfer
+        </button>
+      </td>
+      <td>
+        <button type="button" class="delete-koala" data-id="${koalas[i].id}">
+          Delete
+        </button>
+      </td>
+    `)
+  }
+
+}
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
